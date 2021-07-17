1e3dd61a98a3814d877492dbcf1629457589d373
Hirochika Yuda <hyuda@outlook.jp>
2021-07-18 00:52:39 +0900

    Fix build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 
 BASEDIR=$(dirname $0)
 
+cd $(mktemp -d)
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.85.0/hugo_extended_0.85.0_Linux-64bit.tar.gz | tar zxv
+cp hugo $BASEDIR/hugo.linux
+
+npm install -g postcss-cli autoprefixer postcss
+
 cd $BASEDIR/content/polaris
 for u in $(cat .imagelist); do
     curl -LO $u
 done
 
 cd $BASEDIR
-hugo
+./hugo.linux
