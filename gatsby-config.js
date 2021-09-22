60bf9408bc94901031f2b1c679c43019e43eb4b1
Hirochika Yuda <hyuda@outlook.jp>
2021-09-23 00:35:45 +0900

    Anti-FLoC Header

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,8 @@ module.exports = {
       resolve: 'gatsby-plugin-gatsby-cloud',
       options: {
         allPageHeaders: [
-          'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload'
+          'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload',
+          'Permissions-Policy: interest-cohort=()',
         ]
       }
     }
