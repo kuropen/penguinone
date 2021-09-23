89fe7661f60847ad7f62d76aa2e51ea9862a13f2
Hirochika Yuda <hyuda@outlook.jp>
2021-09-23 14:13:30 +0900

    Optimize Latin font loading

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { Link, useStaticQuery, graphql } from "gatsby";
 
-import "@fontsource/orbitron/700.css"
 import "@ibm/plex/css/ibm-plex-sans-jp.css"
-import "@ibm/plex/css/ibm-plex.css"
 import "../css/main.css"
 import PenguinImage from "../images/penguin.png"
 
